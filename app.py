71b12613f81532b3af2c270457e82ca66f02e674
Varsha <varsha.singh0497@gmail.com>
2020-12-27 00:41:53 +0530

    link backend and frontend partially

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, request, session
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from tempfile import mkdtemp
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -18,22 +19,39 @@ def index():
     """Redirect user"""
     return redirect("/login")
 
-@app.route("/signup", methods=["GET", "POST"])
-def signup():
-    """Sign up user"""
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
     session.clear()
+    if request.method == "GET":
+        return render_template("register.html")
     
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
     # forget any user_id
     session.clear()
-    return render_template("login.html")
+    
+    # user reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+        # validate username/password
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return "Invalid username or password."
+        
+        session["user_id"] = rows[0]["id"]
+        return "Success!"
+    
+    # user reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
     
 
 @app.route('/tasks')
 def tasks():
     return render_template('tasks.html')
+    """TODO"""
     
 @app.route("/settings")
 def settings():

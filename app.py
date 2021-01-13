f9a69dbb9e0a8ba5292934ef130ceecb7db2ed67
Varsha <varsha.singh0497@gmail.com>
2021-01-14 00:20:58 +0530

    small changes to register and login

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, redirect, request, redirect, url_for, session
+from flask import Flask, flash, render_template, redirect, request, redirect, url_for, session
 from tempfile import mkdtemp
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from helpers import login_required
 
 app = Flask(__name__)
 
@@ -48,11 +49,11 @@ def register():
         
         # ensure username is distinct
         if rows != 0:
-            return "<script>alert('Username is already taken')</script>"
+            flash("Username is already taken")
         
         # check for matching passwords
         elif pass1 != pass2:
-            return "<script>alert('Passwords don't match')</script>"
+            flash("Passwords don't match")
         
         else:
             # adding values to the table
@@ -60,6 +61,7 @@ def register():
             db.session.add(register)
             db.session.commit()
             
+            flash("Registered!")
             return redirect(url_for("login"))
         
     else:
@@ -73,18 +75,33 @@ def login():
     
     # user reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
+        
+        # ensure username was submitted
+        if not request.form.get("username"):
+            flash("You must provide username")
+            return render_template("login.html")
+        
+        # ensure password was submitted
+        elif not request.form.get("password"):
+            flash("You must provide password")
+            return render_template("login.html")
+        
         username = request.form.get("username")
         password = request.form.get("password")
         
-        login = users.query.filter_by(username=username, password=password).first()
-        if login is not None:
+        # query database for user
+        user = users.query.filter_by(username=username, password=password).first()
+        if user is not None:
+        # Remember which user has logged in
+            session["user_id"] = user.id
             return redirect(url_for("tasks"))
-        return "<script>alert('Invalid username/password')</script>"
+        flash("Invalid username/password")
     
     else:
         return render_template("login.html")
 
 @app.route("/tasks", methods=["GET"])
+@login_required
 def tasks():
     """User tasks"""
     # show all todos
@@ -93,6 +110,7 @@ def tasks():
     return render_template("tasks.html", todo_list=todo_list)
 
 @app.route("/add", methods=["POST"])
+@login_required
 def add():
     """Add new item"""
     title = request.form.get("title")
@@ -102,12 +120,23 @@ def add():
     return redirect(url_for("tasks"))
 
 @app.route("/delete/<int:todo_id>")
+@login_required
 def delete(todo_id):
     # delete item
     todo_item = todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo_item)
     db.session.commit()
     return redirect(url_for("tasks"))
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     db.create_all()

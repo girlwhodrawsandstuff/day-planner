6c930a5e0ac2b9c4b68d08c12c4b9945895abe51
Varsha <varsha.aux@redgreen.io>
2021-04-29 13:51:21 +0530

    feat: make calendar functional

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -1,6 +1,34 @@
 const date = new Date();
 
-const months = [
+const renderCalendar = () => {
+  date.setDate(1);
+
+  const daysOfMonth = document.querySelector(".days");
+  const daysOfWeek = 7;
+
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = daysOfWeek - lastDayIndex - 1;
+
+  const months = [
     "January",
     "February",
     "March",
@@ -12,8 +40,44 @@ const months = [
     "September",
     "October",
     "November",
-    "December"
-];
+    "December",
+  ];
 
-document.querySelector('.date h1').innerHTML
-= months[date.getMonth()];
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+  document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+  let days = "";
+
+  for (let eachPrevDay = firstDayIndex; eachPrevDay > 0; eachPrevDay--) {
+    days += `<div class="prev-date">${prevLastDay - eachPrevDay + 1}</div>`;
+  }
+
+  for (let eachDay = 1; eachDay <= lastDay; eachDay++) {
+    if (
+      eachDay === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class="today">${eachDay}</div>`;
+    } else {
+      days += `<div>${eachDay}</div>`;
+    }
+  }
+
+  for (let eachNextDay = 1; eachNextDay <= nextDays; eachNextDay++) {
+    days += `<div class="next-date">${eachNextDay}</div>`;
+    daysOfMonth.innerHTML = days;
+  }
+};
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
+
+renderCalendar();

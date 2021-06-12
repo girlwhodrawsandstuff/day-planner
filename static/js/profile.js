e43f90292f7ec7721ebaa855c70a51f0edfeee46
Varsha Singh <varsha.singh0497@gmail.com>
2021-06-12 17:17:33 +0530

    refactor: move all JS to external file

---
+++ static/js/profile.js
@@ -0,0 +1,20 @@
+const wrapper = document.querySelector(".wrapper");
+      const defaultBtn = document.querySelector("#default-btn");
+      const customBtn = document.querySelector("#custom-btn");
+      const img = document.querySelector("img");
+      let regExp = /[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_ ]+$/;
+      function defaultBtnActive() {
+        defaultBtn.click();
+      }
+      defaultBtn.addEventListener("change", function() {
+        const file = this.files[0];
+        if(file) {
+          const reader = new FileReader();
+          reader.onload = function() {
+            const result = reader.result;
+            img.src = result;
+            wrapper.classList.add("active");
+          }
+          reader.readAsDataURL(file);
+        }
+      });

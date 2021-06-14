b7cdb3e695fe7c625d5460541e3057f9315de890
Varsha Singh <varsha.singh0497@gmail.com>
2021-06-14 13:23:45 +0530

    test: test status code for `/login`

--- test.py
+++ test.py
@@ -8,5 +8,12 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 302)
 
+    # login route should return OK
+    def test_login(self):
+        tester = app.test_client(self)
+        response = tester.get('/login', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+    
+
 if __name__ == "__main__":
     unittest.main()
